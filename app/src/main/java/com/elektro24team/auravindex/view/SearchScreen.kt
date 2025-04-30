8054621a9dada577a896a73952133d7aeb7e7880
AlejandroFV74 <00067323@uca.edu.sv>
2025-04-30 14:07:01 -0600

    Preview added for search screen

--- app/src/main/java/com/elektro24team/auravindex/view/SearchScreen.kt
+++ app/src/main/java/com/elektro24team/auravindex/view/SearchScreen.kt
@@ -8,12 +8,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.elektro24team.auravindex.ui.components.BottomNavBar
 import com.elektro24team.auravindex.ui.components.DrawerMenu
 import com.elektro24team.auravindex.ui.theme.MediumPadding
 import kotlinx.coroutines.launch
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.elektro24team.auravindex.navigation.Routes
 import com.elektro24team.auravindex.ui.components.ShowExternalLinkDialog
 import com.elektro24team.auravindex.utils.hamburguerMenuNavigator
@@ -93,4 +95,12 @@ fun SearchScreen(navController: NavController ) {
             }
         )
     }
+}
+
+//función sin parámetro navController para usar preview
+@Preview(showBackground = true)
+@Composable
+fun previewSearchScreen(){
+    val navController = rememberNavController()
+    SearchScreen(navController)
 }
