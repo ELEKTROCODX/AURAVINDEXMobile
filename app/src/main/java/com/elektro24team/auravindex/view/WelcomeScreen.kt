edc857bf751bbd7cc38ae4476a548323454d057f
Alex22sv <ae22mp@gmail.com>
2025-05-09 21:08:11 -0600

    Added back hidden pages to continue testing

--- app/src/main/java/com/elektro24team/auravindex/view/WelcomeScreen.kt
+++ app/src/main/java/com/elektro24team/auravindex/view/WelcomeScreen.kt
@@ -66,7 +66,7 @@ fun WelcomeScreen(
         modifier = Modifier
             .clickable {
                 if(isReadyToNavigate) {
-                    navController.navigate(/*Routes.LOGIN*/ Routes.MAIN) {
+                    navController.navigate(Routes.LOGIN) {
                         popUpTo(Routes.WELCOME) {
                             inclusive = true
                         }
