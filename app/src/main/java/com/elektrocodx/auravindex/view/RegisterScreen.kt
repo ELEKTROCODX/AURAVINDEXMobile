d82b93eaf1344c36003d600710760d361e9efd4b
Alex22sv <ae22mp@gmail.com>
2025-07-01 07:23:36 -0600

    Hide password in register screen

--- app/src/main/java/com/elektrocodx/auravindex/view/RegisterScreen.kt
+++ app/src/main/java/com/elektrocodx/auravindex/view/RegisterScreen.kt
@@ -51,6 +51,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
@@ -303,6 +305,7 @@ fun RegisterScreen(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(vertical = 6.dp),
+                            visualTransformation = if(label == "Password") PasswordVisualTransformation() else VisualTransformation.None,
                             shape = RoundedCornerShape(16.dp),
                             colors = fieldColors
                         )
