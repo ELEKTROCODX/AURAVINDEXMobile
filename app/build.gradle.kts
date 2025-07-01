5cb5d7daa6caebda945b0e5adb40e62c5d0228df
Alex22sv <ae22mp@gmail.com>
2025-06-30 19:01:32 -0600

    Preparing version 4.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.elektro24team.auravindex"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8 // Reminder: Always increment this when updating the version
-        versionName = "3.0.1" // X.Y.Z; X = Major, Y = Minor, Z = Patch level
+        versionCode = 9 // Reminder: Always increment this when updating the version
+        versionName = "4.0.0" // X.Y.Z; X = Major, Y = Minor, Z = Patch level
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
